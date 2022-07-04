068dd38168f4eb2f38c1afedac7482846ef5bdc8
raider_pel <bugrakutik@gmail.com>
2022-07-04 21:47:55 +0300

    Öğrenci Classı ilk işlemler yapıldı

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,5 +1,9 @@
 public class Ogrenci {
     public static void main(String[] args) {
-        System.out.println("merhaba dünya");
+
+
+        System.out.println("Merhaba dünya 2");
+        System.out.println("Merhaba dünya 3");
+
     }
 }
